6a989900c664f6a31076d0c0713b5d60e376d743
catd825 <60204063+catd825@users.noreply.github.com>
2021-08-05 21:12:45 -0400

    added model and controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,15 @@
+class UserController < ApplicationController
+     before_action :authenticate_user!
+
+end
+
+
+# To verify if a user is signed in, use the following helper:
+
+# user_signed_in?
+# For the current signed-in user, this helper is available:
+
+# current_user
+# You can access the session for this scope:
+
+# user_session
